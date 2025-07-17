2abe18a687c7992a25f7a1d808843b3deb69b3d8
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-16 23:54:08 -0300

    adicionando cors na api gw tambem

--- Program.cs
+++ Program.cs
@@ -3,9 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermissiveCors", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-
+app.UseCors("PermissiveCors");
 app.MapReverseProxy();
 
 app.Run();
